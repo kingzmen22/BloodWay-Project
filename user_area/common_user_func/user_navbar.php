3061a09a0491f21d403aac97f55bd7783b418283
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2022-06-21 22:15:30 +0530

    user button with logout and other option included

--- user_area/common_user_func/user_navbar.php
+++ user_area/common_user_func/user_navbar.php
@@ -4,15 +4,16 @@ include('../includes/connect.php');
 
 <header>
     <nav>
-        <input type="checkbox" id="check" />
-        <label for="check" class="checkbtn">
-            <i class="fas fa-bars"></i>
-        </label>
+
         <a id="logo-a-tag" href="../index.php">
             <img id="logo-home" src="../images/logotransparent.png" alt="" />
         </a>
         <div class="nav-drkbtn-ul">
             <img src="../images/moon.png" id="icon" alt="dark mode" data-toggle="tooltip" title="Dark Mode" />
+            <input type="checkbox" id="check" />
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
             <ul>
                 <li><a class="active" href="../index.php">Home</a></li>
                 <li><a href="./reg_form.php">Be A Donor</a></li>
@@ -29,7 +30,8 @@ include('../includes/connect.php');
                 if (!isset($_SESSION['user_name'])) {
                     echo  "<li><a href='./user_login.php'>Login</a></li>";
                 } else {
-                    echo  "<li><a href='./user_logout.php'>Logout</a></li>";
+                    include('../user_area/user_button.php');
+                    // echo  "<li><a href='./user_logout.php'>Logout</a></li>";
                 }
                 ?>
 
