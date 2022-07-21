449daf73c631a16e454df04663720ed83642199a
Alan Varghese Paul <alanvarghesepaul29@gmail.com>
2022-07-22 01:35:31 +0530

    donor searching page designing started

--- user_area/findADonor.php
+++ user_area/findADonor.php
@@ -3,12 +3,17 @@ session_start();
 include('../includes/connect.php');
 
 if (!isset($_SESSION["user_email"])) {
-    // when user is not logged in you cant search a donor
-    // so redirecting to login page
-    header('location:findaDonor_login_redirect.php');
+  // when user is not logged in you cant search a donor
+  // so redirecting to login page
+  header('location:findaDonor_login_redirect.php');
 }
 
-
+if (isset($_SESSION["user_email"])) {
+  $conf_email = $_SESSION['user_email'];
+  $select_query = "Select * from donor_details";
+  $result = mysqli_query($con, $select_query);
+  $rows_count = mysqli_num_rows($result);
+}
 
 ?>
 
@@ -22,219 +27,109 @@ if (!isset($_SESSION["user_email"])) {
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="../css/style.css" />
   <link rel="stylesheet" href="../css/style_signup.css" />
+  <link rel="stylesheet" href="../css/fullbs5.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
 <style>
-  body {
+  /* body {
     background-color: white;
-  }
+  } */
 </style>
 </head>
 
 <body>
   <!-- NavBar -->
   <?php
-include('common_user_func/user_navbar.php');
-?>
+  include('common_user_func/user_navbar.php');
+  ?>
 
+  <!-- searchbar -->
+  <h3 class="mt-3 mx-3">
+    Search for donors here
+  </h3>
 
-  <div class="table-container">
-    <table class="table">
-      <thead>
-        <tr>
-          <th>S.No</th>
-          <th>Name</th>
-          <th>Age</th>
-          <th>Gender</th>
-          <th>Blood Group</th>
-          <th>District/Zone</th>
-          <th>Email</th>
-          <th>Mob No.</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td data-label="S.No">1</td>
-          <td data-label="Name">Dinesh</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
+  <div class="container">
+    <div class="row align-items-start mt-3 ">
+      <div class="col">
+        <label class="">Blood Group</label>
+        <form action="" method="get">
+          <select class="form-select">
+            <option value="" selected>Select</option>
+            <option value="A+">A+</option>
+            <option value="A-">A-</option>
+            <option value="AB+">AB+</option>
+            <option value="AB-">AB-</option>
+            <option value="B+">B+</option>
+            <option value="B-">B-</option>
+            <option value="O+">O+</option>
+            <option value="O-">O-</option>
+            <option value="Bombay blood group">Bombay blood group</option>
+          </select>
+        </form>
+      </div>
 
-        <tr>
-          <td data-label="S.No">2</td>
-          <td data-label="Name">Kamal</td>
-          <td data-label="Age">23</td>
-          <td data-label="Marks%">70%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
+      <div class="col">
+        <label class="">Distirct/Zone</label>
+        <form action="" method="get">
+          <select class="form-select">
+            <option value="" selected>Select</option>
+            <option value="Ernakulam">Ernakulam</option>
+            <option value="Thrissur">Thrissur</option>
+          </select>
+        </form>
+      </div>
+      <h6 class="mt-2">
+        <center>-------OR-------</center>
+      </h6>
+      <div class="col">
+        <input id="search" type="text" class="form-control" placeholder="Search for Names...." data-tables="donors-list">
+      </div>
 
-        <tr>
-          <td data-label="S.No">3</td>
-          <td data-label="Name">Neha</td>
-          <td data-label="Age">20</td>
-          <td data-label="Marks%">90%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">4</td>
-          <td data-label="Name">Priya</td>
-          <td data-label="Age">30</td>
-          <td data-label="Marks%">30%</td>
-          <td data-label="Staus">Failed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-        <tr>
-          <td data-label="S.No">1</td>
-          <td data-label="Name">Dinesh</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">2</td>
-          <td data-label="Name">Kamal</td>
-          <td data-label="Age">23</td>
-          <td data-label="Marks%">70%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">3</td>
-          <td data-label="Name">Neha</td>
-          <td data-label="Age">20</td>
-          <td data-label="Marks%">90%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">4</td>
-          <td data-label="Name">Priya</td>
-          <td data-label="Age">30</td>
-          <td data-label="Marks%">30%</td>
-          <td data-label="Staus">Failed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-        <tr>
-          <td data-label="S.No">1</td>
-          <td data-label="Name">Dinesh</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">2</td>
-          <td data-label="Name">Kamal</td>
-          <td data-label="Age">23</td>
-          <td data-label="Marks%">70%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">3</td>
-          <td data-label="Name">Neha</td>
-          <td data-label="Age">20</td>
-          <td data-label="Marks%">90%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">4</td>
-          <td data-label="Name">Priya</td>
-          <td data-label="Age">30</td>
-          <td data-label="Marks%">30%</td>
-          <td data-label="Staus">Failed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-        <tr>
-          <td data-label="S.No">1</td>
-          <td data-label="Name">Dinesh</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">2</td>
-          <td data-label="Name">Kamal</td>
-          <td data-label="Age">23</td>
-          <td data-label="Marks%">70%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">3</td>
-          <td data-label="Name">Neha</td>
-          <td data-label="Age">20</td>
-          <td data-label="Marks%">90%</td>
-          <td data-label="Staus">Passed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-
-        <tr>
-          <td data-label="S.No">4</td>
-          <td data-label="Name">Priya</td>
-          <td data-label="Age">30</td>
-          <td data-label="Marks%">30%</td>
-          <td data-label="Staus">Failed</td>
-          <td data-label="Age">34</td>
-          <td data-label="Marks%">50%</td>
-          <td data-label="Staus">Passed</td>
-        </tr>
-      </tbody>
-    </table>
+    </div>
   </div>
 
+
+
+  <!-- table -->
+  <?php
+
+  if ($rows_count < 1) { ?>
+    <h4 class="norecord-findon">No records found!</h4>
+  <?php
+  } else { ?>
+    <table class="table mt-5 table-dark table-hover table-responsive donors-list">
+      <thead>
+        <tr>
+          <th>SI No.</th>
+          <th>Name</th>
+          <th>Blood Group</th>
+          <th>Distirct/Zone</th>
+          <th>Status</th>
+          <th>Contact</th>
+        </tr>
+      </thead>
+
+      <tbody id="tab">
+        <?php
+        $si_no=1;
+        while ($fetchData = mysqli_fetch_assoc($result)) : ?>
+          <tr>
+            <th><?php echo $si_no; ?></th>
+            <td><?php echo $fetchData['donor_name']; ?></td>
+            <td><?php echo $fetchData['donor_bgrp']; ?></td>
+            <td><?php echo $fetchData['donor_zone']; ?></td>
+          </tr>
+        <?php $si_no++; endwhile; ?>
+      </tbody>
+
+    </table>
+  <?php } ?>
   <!-- footer -->
 
   <?php
-include('common_user_func/user_footer.php');
-?>
+  include('common_user_func/user_footer.php');
+  ?>
 
 
 </body>
@@ -276,6 +171,17 @@ include('common_user_func/user_footer.php');
   }
 </script>
 
+<!-- jquery script for searchbar  -->
+<script>
+  $(document).ready(function() {
+    $("#search").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#tab tr").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
+</script>
 
 
 </html>
